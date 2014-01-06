c05b088e075db102148a30c143b09acf9b4ce5d8
vince <chuckySTAR@gmail.com>
2014-01-06 10:37:37 +0100

    fixed contact

--- frontend/controllers/contact.js
+++ frontend/controllers/contact.js
@@ -1,7 +1,6 @@
 var xmpp = angular.module('xmpp');
 
 // used to only register once to the message event since the controller is called on every contact change
-var init = false;
 
 xmpp.controller('contactController', ['$scope', '$stateParams', 'utils', 'xmpp', function ($scope, $stateParams, utils, xmpp) {
     $scope.contact = utils.findById($scope.contacts, $stateParams.contactJid);
@@ -9,23 +8,20 @@ xmpp.controller('contactController', ['$scope', '$stateParams', 'utils', 'xmpp',
 
     $scope.contact.unread = 0;
 
-    if (!init) {
-        init = true;
-        var api = xmpp.getApi();
-        api.on('message', function(from, message) {
-            // only add new messages from our partner
-            if (from != $scope.contact.jid) {
-                return;
-            }
+    var api = xmpp.getApi();
+    api.on('message', function(from, message) {
+        // only add new messages from our partner
+        if (from != $scope.contact.jid) {
+            return;
+        }
 
-            $scope.messages.push({
-                jid: from,
-                body: message.body,
-                time: message.time
-            });
-            $scope.$apply();
+        $scope.messages.push({
+            jid: from,
+            body: message.body,
+            time: message.time
         });
-    }
+        $scope.$apply();
+    });
 
     $scope.send = function() {
         xmpp.sendMessage($scope.contact.jid, $scope.msg);
